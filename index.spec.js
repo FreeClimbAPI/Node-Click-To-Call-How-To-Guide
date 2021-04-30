7aca6497395691b08d24a8a65137bf432d2a32c4
Matt Rohan <mrohan@vailsys.com>
2021-04-30 12:46:04 -0500

    add sample test for cov reporting

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,17 @@
-const { it, expect } = require('@jest/globals')
+let request
 
-describe('example test', () => {
-    it('evaluates truthiness', () => {
-        expect(true).toBeTruthy()
+beforeEach(() => {
+    jest.resetModules()
+    const { app } = require('./index')
+    const supertest = require('supertest')
+    request = supertest(app)
+})
+
+describe('GET /', () => {
+    it('renders the inputPhone template', async () => {
+        const res = await request.get('/')
+        expect(res.status).toBe(200)
     })
 })
+
+
